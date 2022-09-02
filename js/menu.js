87bf794c18c6360de754981f1cca75492145bca5
egonzalezdavinci <72173667+egonzalezdavinci@users.noreply.github.com>
2022-09-02 10:51:05 -0300

    Update init
    Empezando proyecto

---
+++ js/menu.js
@@ -0,0 +1,29 @@
+let menu = `<header>
+    <div>
+      <h1><a href="index.html" >joaquin <br>gonzalez</a></h1>
+    </div>
+    <div>
+      <img src="img/icono/menu.png" alt="menu" id="menuOpen" class="active" />
+	  <img src="img/icono/menu_cerrar.jpg" alt="menu" id="menuClosed" class="noActive" />
+      <ul class="menu-lista menu-none" id="menu-header">
+        <div class="respmenu">
+          <nav>
+            <ul>
+              <li><a href="index.html">Inicio</a></li>
+              <li><a href="estrenados.html">Estrenados</a></li>
+              <li><a href="salas.html">Salas</a></li>
+              <li><a href="comunicacion.html">Comunicación</a></li>
+              <li><a href="estreno.html">Estreno</a></li>
+			  <li><a href="cartelera.html">cartelera</a></li>
+			  <li><a href="miscompras.html">mis compras</a></li>
+			  <li><a href="cumpleanos.html">cumpleaños</a></li>
+			  <li><a href="promociones.html">Promosiones</a></li>
+			  <li><a href="iniciar-sesion.html">iniciar sesion</a></li>
+			  <li><a href="joaco-fan.html">joaco fan</a></li>
+			  <li><a href="jugeteria.html">jugeteria</a></li>
+            </ul>
+          </nav>
+        </div>
+      </ul>
+    </div>
+  </header>`;
